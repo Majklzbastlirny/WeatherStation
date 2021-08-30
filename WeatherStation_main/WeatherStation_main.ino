383770650f9f162d3d50dc755c3616272276ed84
Majklzbastlirny <55651520+Majklzbastlirny@users.noreply.github.com>
2021-08-30 20:24:06 +0200

    Created MAIN file
    This file will be used in main Weather Station

---
+++ WeatherStation_main/WeatherStation_main.ino
@@ -0,0 +1,13 @@
+//Majkl's Weather Station with ESP32, MQTT, BMP180, BH1750, DHT22, ML8511, Wind vane, Rain gauge and Anemometer.
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
