667b6774fa1ffd77bbea7b31a801e6baa15a2b65
Majklzbastlirny <55651520+Majklzbastlirny@users.noreply.github.com>
2021-10-07 15:36:31 +0200

    Update ML8511-WORKING.ino

--- Samples/ML8511-WORKING/ML8511-WORKING.ino
+++ Samples/ML8511-WORKING/ML8511-WORKING.ino
@@ -1,38 +1,4 @@
- /* 
- ML8511 UV Sensor Read Example
- By: Nathan Seidle
- SparkFun Electronics
- Date: January 15th, 2014
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
- The ML8511 UV Sensor outputs an analog signal in relation to the amount of UV light it detects.
-
- Connect the following ML8511 breakout board to Arduino:
- 3.3V = 3.3V
- OUT = A0
- GND = GND
- EN = 3.3V
- 3.3V = A1
- These last two connections are a little different. Connect the EN pin on the breakout to 3.3V on the breakout.
- This will enable the output. Also connect the 3.3V pin of the breakout to Arduino pin 1.
-
- This example uses a neat trick. Analog to digital conversions rely completely on VCC. We assume
- this is 5V but if the board is powered from USB this may be as high as 5.25V or as low as 4.75V:
- http://en.wikipedia.org/wiki/USB#Power Because of this unknown window it makes the ADC fairly inaccurate
- in most cases. To fix this, we use the very accurate onboard 3.3V reference (accurate within 1%). So by doing an
- ADC on the 3.3V pin (A1) and then comparing this against the reading from the sensor we can extrapolate
- a true-to-life reading no matter what VIN is (as long as it's above 3.4V).
-
- Test your sensor by shining daylight or a UV LED: https://www.sparkfun.com/products/8662
-
- This sensor detects 280-390nm light most effectively. This is categorized as part of the UVB (burning rays)
- spectrum and most of the UVA (tanning rays) spectrum.
-
- There's lots of good UV radiation reading out there:
- http://www.ccohs.ca/oshanswers/phys_agents/ultravioletradiation.html
- https://www.iuva.org/uv-faqs
-
-*/
+ 
 
 //Hardware pin definitions
  int UVOUT = 32; //Output from the sensor
