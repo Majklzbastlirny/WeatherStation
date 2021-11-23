1737c3e74b206f83a9068a58e9cc9fe01193e162
Majklzbastlirny <55651520+Majklzbastlirny@users.noreply.github.com>
2021-11-23 21:10:29 +0100

    Update Combined.ino

--- Combined/Combined.ino
+++ Combined/Combined.ino
@@ -44,12 +44,12 @@ Adafruit_MQTT_Publish WS = Adafruit_MQTT_Publish(&mqtt, "weatherStation/Speed");
 
 /******************* Globální proměnné, definice a objekty **************************************/
 //Seriový výstup zapnut
-#define Sprintln(a) (Serial.println(a))
-#define Sprint(a) (Serial.print(a))
+//#define Sprintln(a) (Serial.println(a))
+//#define Sprint(a) (Serial.print(a))
 
 //Seriový výstup vypnut
-//#define Sprintln(a)
-//#define Sprint(a)
+#define Sprintln(a)
+#define Sprint(a)
 
 
 #define DOBA_HIBERNACE 10 //v sekundách
