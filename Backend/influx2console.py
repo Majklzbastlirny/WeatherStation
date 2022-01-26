02b0f780a17ef86fa9c28dd349d76041053fd85b
Majklzbastlirny <minecz.michal.basler@gmail.com>
2022-01-26 21:14:55 +0100

    Created python script
    It's for pulling latest data and showing them. later this will be displaying results as JSON

---
+++ Backend/influx2console.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# You can generate an API token from the "API Tokens Tab" in the UI
+token = "H4hYgXBEUTFRivBBLdo0Pk_ca1qCAmN8O-LRGjRLRoVitVGcSfervwQQOKa8Jka7rwDa2m7EDlqZn_MmNqsezQ=="
+org = "MajklovaBastlirna"
+bucket = "WeatherStation"
+query = 'from(bucket: "WeatherStation") |> range(start: -1m) |> last()'
+
+client =  InfluxDBClient(url="http://192.168.0.8:8086", token=token, org=org)
+      
+result = client.query_api().query(query, org=org)
+
+results = []
+for table in result:
+    for record in table.records:
+        results.append((record.get_measurement(), record.get_value()))
+
+print(results)
