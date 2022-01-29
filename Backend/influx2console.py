5fee50fa705b6c75ab2733fbbff851d264bc3cc3
Majklzbastlirny <minecz.michal.basler@gmail.com>
2022-01-29 20:51:03 +0100

    Added ignore for worthless metrics

--- Backend/influx2console.py
+++ Backend/influx2console.py
@@ -5,7 +5,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 token = "H4hYgXBEUTFRivBBLdo0Pk_ca1qCAmN8O-LRGjRLRoVitVGcSfervwQQOKa8Jka7rwDa2m7EDlqZn_MmNqsezQ=="
 org = "MajklovaBastlirna"
 bucket = "WeatherStation"
-query = 'from(bucket: "WeatherStation") |> range(start: -1m) |> last()'
+query = 'from(bucket: "WeatherStation") |> range(start: -1m) |> last() |> filter(fn: (r) => r._measurement != "HeaterStatus" and r._measurement != "Voltage")'
 
 client =  InfluxDBClient(url="http://bladyhel.serveminecraft.net:8086", token=token, org=org)
       
@@ -15,6 +15,7 @@ results = []
 for table in result:
     for record in table.records:
         results.append((record.get_measurement(), record.get_value()))
+
 
 listToStr = '-'.join([str(elem) for elem in results])
 listToStr = listToStr.replace(",", ":")
